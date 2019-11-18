eb2c1cf58889b8e68859fe3c74ac604420c5bf6f
Jatin <jatin.mca17.du@gmail.com>
2019-11-18 12:27:08 +0530

    number plate detection with matlab

--- numberplate_detection/numberplate.m
+++ numberplate_detection/numberplate.m
@@ -1,48 +1,86 @@
+%  LICENSE PLATE DETECTION
+
+% read image
 img = imread("1.jpg");
-procimg = rgb2gray(img);
 
-thresh = graythresh(procimg);
+% convert the truecolor image RGB to the grayscale intensity image
+grayimg = rgb2gray(img);
 
-edgeDetectedImage = edge(procimg, 'Sobel');
+% computes a global threshold T from grayscale image
+threshold = graythresh(grayimg);
 
-imshow(edgeDetectedImage);
+%%%%%%%%%%%%%% STEP 1 : Vertical edge detection %%%%%%%%%%%%%% 
 
-[height, width] = size(procimg);
+% Do a Sobel edge detection
+edgeDetectedImage = edge(grayimg, 'Sobel');
 
-SE = strel('rectangle', [1,50]);
-closeProcImg = imclose(edgeDetectedImage, SE);
+% display image
+% imshow(edgeDetectedImage);
 
-SE = strel('rectangle', [30,1]);
-openProcImg1 = imopen(closeProcImg, SE);
+% get size of grayscale img
+[imageHeight, imageWidth] = size(grayimg);
 
-SE = strel('rectangle', [100,1]);
-openProcImg2 = imopen(openProcImg1, SE);
+%%%%%%%%%%%%%%% STEP 2 : Perform morphological operations %%%%%%%%%%%%%% 
 
-subimg = zeros(height, width);
-for i = 1 : height
-    for j = 1 : width
-        subimg(i,j) = openProcImg1(i,j) - openProcImg2(i,j);
+% create a structuring element of shape rectangle with 1 row and 50 columns
+structuringElement = strel('rectangle', [1, 50]);
+
+% Perform morphological closing of the edge detection 
+closedImage = imclose(edgeDetectedImage, structuringElement);
+
+% create a structuring element of shape rectangle with 30 rows and 1 columns
+structuringElement = strel('rectangle', [30, 1]);
+
+% Perform morphological opening
+openedImage1 = imopen(closedImage, structuringElement);
+
+% create a structuring element of shape rectangle with 100 rows and 1 columns
+structuringElement = strel('rectangle', [100, 1]);
+
+% Perform morphological opening
+openedImage2 = imopen(openedImage1, structuringElement);
+
+% create a matrix with all zeroes
+subimg = zeros(imageHeight, imageWidth);
+
+for i = 1 : imageHeight
+    for j = 1 : imageWidth
+        subimg(i,j) = openedImage1(i,j) - openedImage2(i,j);
     end
 end
 
-SE = strel(1,100);
-subimg = imopen(subimg,SE);
+% create a structuring element
+structuringElement = strel(1, 100);
 
-for i = 1 : height
-    for j = 1 : width
-        procimg(i,j) = procimg(i,j) * subimg(i,j);
+% Perform morphological opening
+subimg = imopen(subimg,structuringElement);
+
+for i = 1 : imageHeight
+    for j = 1 : imageWidth
+        grayimg(i,j) = grayimg(i,j) * subimg(i,j);
     end
 end
 
-cc = bwconncomp(procimg, 8);
-stats = regionprops(cc, 'Perimeter');
-idx = find([stats.Perimeter] > 800);
-finalimg = double(ismember(labelmatrix(cc), idx));
+%%%%%%%%%%%%%%%% STEP 3 : Find connected component %%%%%%%%%%%%%%%
 
-for i = 1 : height
-    for j = 1 : width
-        procimg(i,j) = procimg(i,j) * finalimg(i,j);
+% Find connected objects - 8 way connectivity
+connectedComponents = bwconncomp(grayimg, 8);
+
+% Compute properties of image regions.
+properties = regionprops(connectedComponents, 'Perimeter');
+
+% Return a vector of indices where property parameter is >800
+idx = find([properties.Perimeter] > 800);
+
+% Create labelled matrix from bwconncomp structure, then
+% returns a logical matrix with the same shape as original matrix
+finalImage = double(ismember(labelmatrix(connectedComponents), idx));
+
+for i = 1 : imageHeight
+    for j = 1 : imageWidth
+        grayimg(i,j) = grayimg(i,j) * finalImage(i,j);
     end
 end
 
-imshow(procimg);
+% display image
+imshow(grayimg);
